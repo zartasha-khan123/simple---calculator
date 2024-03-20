b2de5ce21e26d0fb746ddcccc33e4924b581abf0
zartasha-khan123 <zartashimran12345@gmail.com>
2024-03-20 05:12:42 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+"# simple---calculator" 
+"# simple---calculator" 
+"# simple---calculator" 
---
+++ index.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+
+// name . shabang
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "enter first number", type: "number", name: "firstNumber" },
+  { message: "enter second number", type: "number", name: "secondNumber" },
+  {
+    message: "select one of the operators to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["addition", "subtraction", "multiplication", "division"],
+  },
+]);
+
+// conditional statement
+
+if (answer.operator === "addition") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "subtraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "multiplication") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+} else {
+  console.log("please enter valid number");
+}
